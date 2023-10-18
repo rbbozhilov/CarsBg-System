7c94877ec021e12a7f2a56e45ab3083f3d5d794c
rbbozhilov <rbbojilov@gmail.com>
2023-10-18 21:55:59 +0300

    Full test on Brand and Category services

---
+++ CarsBg-System.Tests/Services/BrandServiceTest.cs
@@ -0,0 +1,188 @@
+﻿using CarsBg_System.Areas.Admin.Models.Brand;
+using CarsBg_System.Services.Brand;
+using CarsBg_System.Tests.Mock;
+
+using Xunit;
+
+
+namespace CarsBg_System.Tests.Services
+{
+    public class BrandServiceTest
+    {
+        [Fact]
+        public async Task AddBrand_ShouldReturnTrue()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+            var information = new BrandFormModel() { Name = "Test" };
+
+
+            //Act
+
+            bool isAdded = await brandService.AddAsync(information);
+            var result = data.Brands.Any(x => x.Name == "Test");
+
+            //Assert
+
+            Assert.True(isAdded);
+            Assert.True(result);
+
+        }
+
+        [Fact]
+        public async Task AddBrand_ShouldReturnFalse_SameBrandAddedAgain()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+            var information = new BrandFormModel() { Name = "Test" };
+
+
+            //Act
+
+            await brandService.AddAsync(information);
+            bool result = await brandService.AddAsync(information);
+
+            //Assert
+
+            Assert.False(result);
+
+        }
+
+        [Fact]
+        public async Task DeleteBrand_ShouldReturnTrueAndDelete()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+            var information = new BrandFormModel() { Name = "Test" };
+
+
+
+            //Act
+
+            await brandService.AddAsync(information);
+            var brand = data.Brands.Where(x => x.Name == "Test").FirstOrDefault();
+            bool isDeleted = await brandService.DeleteAsync(brand.Id);
+
+
+            //Assert
+
+            Assert.True(isDeleted);
+            Assert.True(brand.IsDeleted);
+        }
+
+        [Fact]
+        public async Task DeleteBrand_ShouldFalse_NotExistBrand()
+        {
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+
+            //Act & Assert
+
+            Assert.False(await brandService.DeleteAsync(2));
+        }
+
+        [Fact]
+        public async Task DeleteBrand_ShouldReturnFalse_BrandIsAlreadyDeleted()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+            var information = new BrandFormModel() { Name = "Test" };
+
+
+            //Act
+
+            await brandService.AddAsync(information);
+            var brand = data.Brands.Where(x => x.Name == "Test").FirstOrDefault();
+            await brandService.DeleteAsync(brand.Id);
+
+            bool result = await brandService.DeleteAsync(brand.Id);
+
+            //Assert
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ReturnAllBrands()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+            var brand1 = new BrandFormModel() { Name = "Test" };
+            var brand2 = new BrandFormModel() { Name = "Test1" };
+            var brand3 = new BrandFormModel() { Name = "Test2" };
+
+
+            //Act
+
+            await brandService.AddAsync(brand1);
+            await brandService.AddAsync(brand2);
+            await brandService.AddAsync(brand3);
+            var result = brandService.ShowBrands();
+            var isHaveBrand1 = result.Any(x => x.Name == "Test");
+            var isHaveBrand2 = result.Any(x => x.Name == "Test1");
+            var isHaveBrand3 = result.Any(x => x.Name == "Test2");
+
+            //Assert
+
+            Assert.Equal(3, result.Count());
+            Assert.True(isHaveBrand3);
+            Assert.True(isHaveBrand2);
+            Assert.True(isHaveBrand1);
+        }
+
+        [Fact]
+        public async Task HaveBrandId_ShouldReturnTrue()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+            var information = new BrandFormModel() { Name = "Test" };
+
+
+            //Act
+
+            await brandService.AddAsync(information);
+            var brandId = data.Brands.Where(x => x.Name == "Test").Select(x => x.Id).FirstOrDefault();
+
+            bool result = brandService.IsHaveBrandById(brandId);
+
+            //Assert
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task HaveBrandId_ShouldReturnFalse_DontHaveThisBrandId()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var brandService = new BrandService(data);
+
+            //Act & Assert
+
+            Assert.False(brandService.IsHaveBrandById(2));
+        }
+
+    }
+}
---
+++ CarsBg-System.Tests/Services/CategoryServiceTest.cs
@@ -0,0 +1,207 @@
+﻿using CarsBg_System.Areas.Admin.Models.Category;
+using CarsBg_System.Services.Category;
+using CarsBg_System.Tests.Mock;
+
+using Xunit;
+
+namespace CarsBg_System.Tests.Services
+{
+    public class CategoryServiceTest
+    {
+
+        [Fact]
+        public async Task AddCategory_ShouldReturnTrue()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+            var information = new CategoryFormModel() { Name = "Test" };
+
+
+            //Act
+
+            await categoryService.AddAsync(information);
+            bool result = data.Categories.Any(x => x.Name == "Test");
+
+            //Assert
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task AddCategory_ShouldReturnFalse_HaveSameCategory()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+            var information = new CategoryFormModel() { Name = "Test" };
+
+
+            //Act
+
+            await categoryService.AddAsync(information);
+            bool result = await categoryService.AddAsync(information);
+
+            //Assert
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task DeleteCategory_ShouldReturnTrueAndDelete()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+            var information = new CategoryFormModel() { Name = "Test" };
+
+
+
+            //Act
+
+            await categoryService.AddAsync(information);
+            var category = data.Categories.Where(x => x.Name == "Test").FirstOrDefault();
+            bool isDeleted = await categoryService.DeleteAsync(category.Id);
+
+
+            //Assert
+
+            Assert.True(isDeleted);
+            Assert.True(category.IsDeleted);
+        }
+
+        [Fact]
+        public async Task DeleteCategory_ShouldFalse_NotExistCategory()
+        {
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+
+            //Act & Assert
+
+            Assert.False(await categoryService.DeleteAsync(2));
+        }
+
+        [Fact]
+        public async Task DeleteCategory_ShouldReturnFalse_CategoryIsAlreadyDeleted()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+            var information = new CategoryFormModel() { Name = "Test" };
+
+
+            //Act
+
+            await categoryService.AddAsync(information);
+            var category = data.Categories.Where(x => x.Name == "Test").FirstOrDefault();
+            await categoryService.DeleteAsync(category.Id);
+
+            bool result = await categoryService.DeleteAsync(category.Id);
+
+            //Assert
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ShowAllCategories_ShouldBeCorrect()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+            var category1 = new CategoryFormModel() { Name = "Test" };
+            var category2 = new CategoryFormModel() { Name = "Test2" };
+            var category3 = new CategoryFormModel() { Name = "Test3" };
+
+
+            //Act
+
+            await categoryService.AddAsync(category1);
+            await categoryService.AddAsync(category2);
+            await categoryService.AddAsync(category3);
+
+            var result = categoryService.ShowCategories();
+
+
+            //Assert
+
+            Assert.Equal(3, result.Count());
+        }
+
+        [Fact]
+        public async Task HaveCategoryById_ReturnTrue()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+            var category1 = new CategoryFormModel() { Name = "Test" };
+
+            //Act
+
+            await categoryService.AddAsync(category1);
+            var currentCategoryId = data.Categories.Where(x => x.Name == "Test").Select(x => x.Id).FirstOrDefault();
+            var result = categoryService.IsHaveCategoryById(currentCategoryId);
+
+
+            //Assert
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task HaveCategoryById_ReturnFalse()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+
+            //Act & Assert
+
+            Assert.False(categoryService.IsHaveCategoryById(5));
+        }
+
+        [Fact]
+        public async Task GetAllCategories_ShouldBeCorrect()
+        {
+
+            //Arrange
+
+            using var data = DatabaseMock.Instance;
+            var categoryService = new CategoryService(data);
+            var category1 = new CategoryFormModel() { Name = "Test" };
+            var category2 = new CategoryFormModel() { Name = "Test2" };
+            var category3 = new CategoryFormModel() { Name = "Test3" };
+
+
+            //Act
+
+            await categoryService.AddAsync(category1);
+            await categoryService.AddAsync(category2);
+            await categoryService.AddAsync(category3);
+
+            var result = categoryService.GetAllCategories();
+
+
+            //Assert
+
+            Assert.Equal(3, result.Count());
+        }
+
+    }
+}
